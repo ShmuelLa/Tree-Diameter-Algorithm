9525c996ee93a58ac1afe58d76fc0d6e0416a1bc
ShmueLa <shmuel516@gmail.com>
2021-06-05 20:33:12 +0300

    v 1.1 minor memory improvements

--- src/Diameter.java
+++ src/Diameter.java
@@ -1,47 +1,43 @@
 import java.util.Vector;
 
 public class Diameter {
-    private boolean[][] mat;
+    Vector[] _tree;
     int _result;
     
     public Diameter(boolean[][] adj_mat) {
-        mat = adj_mat;
-        int nodes = mat.length;
-        var tree = new Vector[nodes];
+        int nodes = adj_mat.length;
+        _tree = new Vector[nodes];
         for (int i = 0; i < nodes; i++) {
-            tree[i] = new Vector(nodes);
+            _tree[i] = new Vector(nodes);
         }
         for (int i = 0; i < nodes; i++) {
             for (int j = i+1; j < nodes; j++) {
-                if (mat[i][j]) {
-                    tree[i].add(j);
-                    tree[j].add(i);
+                if (adj_mat[i][j]) {
+                    _tree[i].add(j);
+                    _tree[j].add(i);
                 }
             }
         }
-        _result = burn_and_calculate(tree);
     }
 
-    public static int burn_and_calculate(Vector[] tree) {
-        int nodes = tree.length;
+    public void burn_and_calculate() {
+        int nodes = _tree.length;
         int vertex = 0;
         int leaf = 0;
         Vector<Integer> leaves = new Vector<>(nodes);
-        int radius = 0, diameter;
+        int radius = 0;
         int[] degrees = new int[nodes];
         for (int i = 0; i < nodes; i++) {
-            degrees[i] = tree[i].size();
-            if (degrees[i] == 1) {
-                leaves.add(i);
-            }
+            degrees[i] = _tree[i].size();
+            if (degrees[i] == 1) leaves.add(i);
         }
         while (nodes > 2) {
             Vector<Integer> temp = new Vector<>(nodes);
             for (Integer integer : leaves) {
                 leaf = integer;
                 degrees[leaf] = 0;
-                for (int j = 0; j < tree[leaf].size(); j++) {
-                    vertex = (int) tree[leaf].get(j);
+                for (int j = 0; j < _tree[leaf].size(); j++) {
+                    vertex = (int) _tree[leaf].get(j);
                     if (degrees[vertex] > 0) {
                         degrees[vertex]--;
                         if (degrees[vertex] == 1) temp.add(vertex);
@@ -54,13 +50,13 @@ public class Diameter {
         }
         if (leaves.size() == 2) {
             radius++;
-            diameter = radius * 2 - 1;
+            _result = radius * 2 - 1;
         }
-        else diameter = radius * 2;
-        return diameter;
+        else _result = radius * 2;
     }
 
     public int get_diam() {
+        burn_and_calculate();
         return _result;
     }
 }
